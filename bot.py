306f84cb5e53fff83a1dde5070d8803059fb98a0
DylanMrr <thefatherofdragons1@gmail.com>
2019-04-13 04:20:10 +0300

    new quest

--- bot.py
+++ bot.py
@@ -36,9 +36,11 @@ def handle_text(message):
         team_finish = dict()
         teams = dict()
     elif message.text.lower() == "getprogress":
-        bot.send_message(message.from_user.id, team_progress)
+        bot.send_message(message.from_user.id, create_progress())
     elif message.text.lower() == 'getresult':
         bot.send_message(message.from_user.id, create_result())
+    elif message.text.lower() == 'getteams':
+        bot.send_message(message.from_user.id, create_teams())
     elif message.text.lower().startswith("id") or teams.get(message.from_user.id) is None:
         if teams.get(message.from_user.id) is None:
         #if message.text.lower().startswith("id"):
@@ -71,7 +73,7 @@ def check_answer(answer, id, id_tg):
     if quest.quest[team_progress[id]][quest.ANSWER] == answer:
         if team_progress[id] == len(quest.quest) - 1:#s
             current_time = datetime.datetime.now()
-            time_str = "{}:{}:{}".format(current_time.hour, current_time.minute, current_time.second)
+            time_str = "{}:{}:{}".format(current_time.hour + 3, current_time.minute, current_time.second)
             team_finish[id] = time_str#
             bot.send_message(id_tg, "Вы правы. Квест завершен, ожидайте результатов")
         else:
@@ -83,12 +85,17 @@ def check_answer(answer, id, id_tg):
 
 def create_result():
     global team_finish
-    #s = ''
-    #for team in team_finish:
-    #    s += "[{}]: {}".format(team, team_finish[team])
-    #return s
     return json.dumps(team_finish)
 
+
+def create_progress():
+    global team_progress
+    return json.dumps(team_progress)
+
+
+def create_teams():
+    global teams
+    return json.dumps(teams)
 
 
 bot.polling(none_stop=True, interval=0)
